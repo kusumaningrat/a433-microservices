18686dd9b50f0f3a242b880ef86eeb7094b351e3
kusumaningrat <kusumanetcom@gmail.com>
2023-08-23 03:05:22 +0000

    Fix submission

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Build docker image with version tag v1 
-docker build -t ghcr.io/kusumaningrat/item-app:v1 .
+# Build docker image
+docker build -t item-app:v1 .
+
+# Listing all docker images
+docker images
+
+# Tagging docker image with ghcr username and version tag v1 
+docker tag item-app:v1 ghcr.io/kusumaningrat/item-app:v1
 
 # Login into container registry repository
 echo $TOKEN | docker login ghcr.io -u $username --password-stdin
