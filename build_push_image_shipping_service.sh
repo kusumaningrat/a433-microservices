b4d25a3c08b9ba5adf9e86970b13d3c01672a34a
Kusuma16 <kusumanetcom@gmail.com>
2023-08-31 12:35:36 +0800

    build and push docker image

---
+++ build_push_image_shipping_service.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# build docker image dari Dockerfile dengan penamaan format
+# Github Packages yaitu shipping-service dengan tag latest
+docker build -t ghcr.io/kusumaningrat/shipping-service .
+
+# otentikasi ke Github Container Registry menggunakan username kusumaningrat
+# beserta token/password yang di ambil dari environment variable
+echo $TOKEN | docker login ghcr.io -u kusumaningrat --password-stdin
+
+# menyimpan/push docker image yang sudah di build ke Github Container Registry
+docker push ghcr.io/kusumaningrat/shipping-service
